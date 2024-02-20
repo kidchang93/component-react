a17da2dd41e33474058efff8fb9941efca7dda0d
kidchang93 <lck930721@gmail.com>
2024-02-20 13:55:03 +0900

    prop

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import MyComponent2 from "./MyComponent2";
 
 const App = () => {
 	return (
+		// App에서 태그와 태그 사이에 데이터를 넘길 수 있다.
 		<div>
-			<MyComponent2 />
+			<MyComponent2 name={3}>Big</MyComponent2>
+			<hr />
+			<MyComponent2 name="SideBar" />
+			<hr />
+			<MyComponent2>
+				<s>리액트</s>입니다.
+			</MyComponent2>
 		</div>
 	);
 };
--- src/MyComponent2.js
+++ src/MyComponent2.js
@@ -1,7 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const MyComponent2 = () => {
-	return <div>컴포넌트</div>;
+const MyComponent2 = ({ name, children }) => {
+	return (
+		<div>
+			{name} 컴포넌트
+			<br />
+			children:{children}
+		</div>
+	);
+};
+
+MyComponent2.defaultProps = {
+	name: "기본 이름",
+};
+
+MyComponent2.propTypes = {
+	name: PropTypes.string.isRequired,
 };
 
 export default MyComponent2;
